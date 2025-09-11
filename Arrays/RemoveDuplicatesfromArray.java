361044ad0e096d5ddb07d9449971a0a5eebb6340
patilrakshita2203-prog <patilrakshita2203@gmail.com>
2025-09-11 13:32:29 +0530

    Add files via upload

---
+++ Arrays/RemoveDuplicatesfromArray.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+public class Main {
+  public static int[] removeDuplicates(int[] array) {
+   int n = array.length;
+   int[] uniqueArray = new int[n];
+   int index = 0;
+   
+   for(int i = 0; i < n; i++ ){
+       boolean isDuplicate = false;
+       for(int j = i+1 ; j<n ; j++){
+           if(array[i] == array[j]){
+               isDuplicate = true;
+           }
+       }
+       if (!isDuplicate){
+           uniqueArray[index++] = array[i];
+       }
+   }
+   return Arrays.copyOf(uniqueArray,index);
+  }
+  public static void main(String[]args){
+      int[] array = {1, 1, 2, 2, 3, 4, 5};
+      int[] Remove = removeDuplicates(array);
+      System.out.println("Duplicate Elements are = " + Remove);
+  }
+}
