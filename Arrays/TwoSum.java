d9b87f473f8dfdae9d859a66365cf20c4d39b839
patilrakshita2203-prog <patilrakshita2203@gmail.com>
2025-09-16 21:43:40 +0530

    Add files via upload

---
+++ Arrays/TwoSum.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+public class Main{
+	public static int[] twoSum(int[] nums , int target){
+	  for(int i=0;i<nums.length;i++){
+	      for(int j=i+1;j<nums.length;j++){
+	          if(nums[i] + nums[j] == target){
+	              return new int[] {i,j};
+	          }
+	      }
+	  }  
+	   throw new IllegalArgumentException("No solution found");
+	}
+	public static void main(String[]args){
+	    int[] nums = {2,7,11,8};
+	    int target = 13;
+	    int[] Sum = twoSum(nums,target);
+	    System.out.println("TWO SUM = " + Arrays.toString(Sum));
+	}
+}
